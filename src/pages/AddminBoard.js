17974671983923403ed60f027d4047312257fa55
Sanjeevani Kandra <sanjeevanikandra@gmail.com>
2025-01-25 12:49:27 +0530

    creat a page fplder

--- src/pages/AddminBoard.js
+++ src/pages/AddminBoard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function AddminBoard() {
   return (
     <div>
-      
+      <h1>Admin Board</h1>
     </div>
   )
 }
--- src/pages/SupervisorBoard.js
+++ src/pages/SupervisorBoard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SupervisorBoard() {
   return (
     <div>
-      
+      <h1>Supervisor Board</h1>
     </div>
   )
 }
