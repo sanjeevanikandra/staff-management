f880e45d654862ce367cf19515fe9e3280a1e236
Chhaya-Verma <chhayav300@gmail.com>
2025-02-01 11:37:31 +0530

    Change the port of SignUp.js file 3000 to 5000

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -31,7 +31,7 @@ function SignUp() {
         role: employeeType
       };
 
-      await fetch('http://localhost:3000/users', {
+      await fetch('http://localhost:5000/users', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
